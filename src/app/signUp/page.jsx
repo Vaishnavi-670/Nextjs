74196da1a65e5935e051892027586ffe2b7f70ec
Vaishnavi-670 <chauhcomanvaishnavi684@gmail.com>
2024-07-29 21:11:22 +0530

    ....

--- src/app/signUp/page.jsx
+++ src/app/signUp/page.jsx
@@ -2,18 +2,49 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-        <h1>Signup</h1>
-      <form>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" required />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" required />
-        <button type="submit">Submit</button>
-      </form>
+    <div className="flex items-center justify-center h-screen   ">
+    <div className="bg-purple-400 fixed w-96 border-[4px] border-purple-600 rounded-2xl hover:border-purple-900 transition-all duration-200">
+      <div className="mx-auto flex items-center space-y-4 py-16 px-12 font-semibold flex-col">
+        <img className='h-24 w-24 rounded-full object-cover' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4_SCRPStWnOp0wyEUVyPtp3_8KrfzcXw7Lg&s'></img>
+       
+        <h1 className="text-2xl">SignUp </h1>
+        <input
+          className="w-full p-2 bg-purple-800 rounded-md border border-purple-700 focus:border-purple-700 hover:border-purple-500 transition-all duration-200"
+          placeholder="Username"
+          type="text"
+          // name="username"
+          />
+        <input
+          className="w-full p-2 bg-purple-800 rounded-md border border-purple-700 focus:border-purple-700 hover:border-purple-500 transition-all duration-200"
+          placeholder="Email"
+          type="email"
+          // name="email"
+          id=""
+        />
+        <input
+          className="w-full p-2 bg-purple-800  rounded-md border border-purple-700 focus:border-purple-700 hover:border-purple-500 transition-all duration-200"
+          placeholder="Password"
+          type="password"
+          // name="password"
+          id=""
+        />
+        <input
+          className="w-full p-2 bg-gray-50 rounded-full font-bold text-gray-900 border-[4px] border-gray-700 hover:border-purple-500 transition-all duration-200"
+          type="submit"
+          id=""
+        />
+        <p>
+          Already have an account?
+          <a
+            className="font-semibold text-white hover:text-purple-800 transition-all duration-200"
+            href="/login"
+          >
+           Login
+          </a>
+        </p>
+      </div>
     </div>
+  </div>
   )
 }
 
