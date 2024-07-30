e8c65990327c1b61f98e6113cae505470972a277
Vaishnavi-670 <chauhcomanvaishnavi684@gmail.com>
2024-07-30 17:30:52 +0530

    ..

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
+        <h1>Contact</h1>
+        <p>Feel free to reach out to me at <a href="mailto:your_email@example.com">your_email@example.com</a></p>
         
     </div>
   )
