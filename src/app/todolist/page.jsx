8b36259cad1a22eb8a813b755ed8c68440567d6b
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-08-03 20:24:57 +0530

    chore: Add functionality to add, delete, and toggle completion of tasks in TodoList

--- src/app/todolist/page.jsx
+++ src/app/todolist/page.jsx
@@ -17,9 +17,29 @@ const TodoList = () => {
     const addTask = (e) => {
         if(e.code === 'Enter'){
             console.log(e.target.value);
+
+            const newTask = {
+                text:e.target.value,
+                completed:false,
+                added:new Date(),
+            }
+            settaskList([...taskList, newTask]);   //...taskList is used to copy the previous taskList element and then add the new task
             e.target.value ='';
         }
     }
+    const deleteTask = (index) => {
+      console.log(index);
+
+      const temp = taskList;
+      temp.splice(index, 1);
+      settaskList([...temp]);  // update the taskList with the new array without changing the reference to the original array.  // React will automatically re-render the component with the updated taskList...
+    }
+    const toggleCompleted =(index) => {
+      const temp = taskList; 
+      temp[index].completed =!temp[index].completed;
+      settaskList([...temp]);  // update the taskList with the new array without changing the reference to the original array.  // React will automatically re-render the component with the updated taskList...
+    }
+      
 
   return (
     <div className='max-w-5xl mx-auto mt-6'>
@@ -33,13 +53,23 @@ const TodoList = () => {
          {
             taskList.map((task , index)=>{
                 return (
-                 <div key={index} className='rounded-md  border mb-5 bg-gray-100 shadpw p-4'>
+                 <div key={index} className='rounded-md  border mb-5  bg-gray-100 shadpw p-4'>
+                  {
+                    task.completed ? 
+                    (<p className='bg-green-500 w-fit text-white text-sm px-2 py-1 rounded-full'>Completed</p>) : 
+                    (<p className='bg-yellow-500 text-white px-2 py-1 w-fit rounded-full '>pending</p>)
+
+                  }
                      <p>{task.text}</p>
                      <div className='flex justify-end gap-4 mt-2'>
-                        <button className='bg-blue-500 px-3 py-1 rounded-full text-white'>Completed</button>
-                        <button className='bg-red-500 px-3 py-1 rounded-full text-white' >Delete</button>
+                        <button onClick={()=> {toggleCompleted(index)}}  className='bg-blue-500 px-3 py-1 rounded-full text-white'>
+                          {
+                            task.completed? 'Mark as Pending' : 'Mark as Completed'
+                          }
+                        </button>
+                        <button onClick={()=> {deleteTask(index)}} className='bg-red-500 px-3 py-1 rounded-full text-white' >Delete</button>
                      </div>
-                 </div>
+                 </div> 
                 )
             })
          }
